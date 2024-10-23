b537bda6d0b8226fb1f32b29516fdd3046f2e932
mathi0101 <mathiasgonzalez0101@gmail.com>
2024-10-23 15:06:13 -0300

    creamos proyecto

---
+++ TrucoRioPlatense/TrucoRioPlatense/Models/Partida/Carta.cs
@@ -0,0 +1,33 @@
+﻿namespace TrucoRioPlatense.Models.Partida {
+    internal class Carta {
+        readonly int[] _numerosCarta = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
+
+
+        public int Numero { get; set; }
+
+        public PaloCarta Palo { get; set; }
+
+
+        public Carta(int numero, PaloCarta palo) {
+            if (!_numerosCarta.Contains(numero)) throw new ExceptionNumeroCartaInvalido();
+            Numero = numero;
+            Palo = palo;
+        }
+
+        public override string ToString() {
+            return $"Carta {Numero} de {Palo}";
+        }
+
+
+
+        public class ExceptionNumeroCartaInvalido : Exception { };
+    }
+
+
+    internal enum PaloCarta {
+        Oro = 0,
+        Copa = 1,
+        Espada = 2,
+        Basto = 3
+    }
+}
---
+++ TrucoRioPlatense/TrucoRioPlatense/Models/Partida/Partida.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrucoRioPlatense.Models.Partida {
+    internal class Partida {
+        public VersionTruco Version { get; set; }
+
+        public int NumeroJugadores {  get; set; }
+
+        public JugadorPartidaTruco[] Jugadores { get; set; } 
+
+
+
+    }
+}
